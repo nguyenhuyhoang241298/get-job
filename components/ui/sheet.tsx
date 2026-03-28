2027498e040f225c1455b91e119029424353f403
nguyenhuyhoang241298 <nguyenhuyhoang241298@gmail.com>
2026-03-28 17:09:58 +0700

    chore: add shadcn/ui sidebar component

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -75,7 +75,8 @@ function SheetContent({
               className="absolute top-3 right-3"
               size="icon-sm"
             >
-              <XIcon />
+              <XIcon
+              />
               <span className="sr-only">Close</span>
             </Button>
           </SheetPrimitive.Close>
